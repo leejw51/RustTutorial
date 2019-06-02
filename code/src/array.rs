61aa39305fe36fd96ebf9abcb52615f2f7b76bda
lee jongwhan <leejw51@gmail.com>
2019-06-02 22:29:42 +0900

    clear

--- code/src/array.rs
+++
@@ -1,7 +0,0 @@
-fn main() 
-{
-    let a = vec![1000,20,300];
-    for i in a.iter() { 
-        println!("{:?}", i);
-    }
-}
--- code/src/iter.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let a: Vec<i32> = vec![0i32, 1, 2];
-
-    let s: Vec<i32> = a.into_iter().filter(|x| x.is_positive()).collect();
-    println!("{:?}", s);
-}
