43790166005d2be946dc7327ae36d09d4ff0389c
Jongwhan Lee <leejw51@gmail.com>
2019-08-17 20:28:48 +0800

    sled

--- test_sled/src/main.rs
+++ test_sled/src/main.rs
@@ -1,3 +1,15 @@
+use sled::Db;
+use std::str;
 fn main() {
-    println!("Hello, world!");
+    let tree = Db::open("./disk").unwrap();
+
+    // set and get
+    tree.insert(b"ok", b"apple".to_vec());
+    tree.insert(b"ok", b"apple5".to_vec());
+    let a= tree.get(b"ok").unwrap().unwrap();
+    let b= str::from_utf8(&a);
+    println!("{:?}",b);
+
+    // block until all operations are on-disk
+    tree.flush();
 }
