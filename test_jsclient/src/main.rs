3d33499109c7ae02d7625e0fca8a8cbda5191cfe
Jongwhan Lee <leejw51@gmail.com>
2019-08-23 00:10:06 +0800

    working

--- test_jsclient/src/main.rs
+++ test_jsclient/src/main.rs
@@ -33,72 +33,16 @@ impl TestClient {
         self.0.call_method("fail", "()", ())
     }
 }
-fn main2() {
-    let a = connect::<TestClient>(CONNECTION);
+fn main() {
+    let client = connect::<TestClient>(CONNECTION)
+        .and_then(|stream| {
+            println!("created stream");
 
-    let mut runtime = tokio::runtime::current_thread::Builder::new()
-        .build()
-        .unwrap();
-    runtime.block_on(a.unwrap());
-
-    // rt::run(run);
-
-    // then
-    // let result = rx.recv_timeout(Duration::from_secs(3)).unwrap();
+            Ok(())
+        })
+        .map_err(|err| {
+            println!("connection error = {:?}", err);
+        });
 
     println!("OK");
-}
-
-fn main() {
-    println!("Connecting to {}", CONNECTION);
-    let mut runtime = tokio::runtime::current_thread::Builder::new()
-        .build()
-        .unwrap();
-
-    // standard in isn't supported in mio yet, so we use a thread
-    // see https://github.com/carllerche/mio/issues/321
-    let (usr_msg, stdin_ch) = mpsc::channel(0);
-    thread::spawn(|| {
-        let mut input = String::new();
-        let mut stdin_sink = usr_msg.wait();
-        loop {
-            input.clear();
-            stdin().read_line(&mut input).unwrap();
-            let trimmed = input.trim();
-
-            let (close, msg) = match trimmed {
-                "/close" => (true, OwnedMessage::Close(None)),
-                "/ping" => (false, OwnedMessage::Ping(b"PING".to_vec())),
-                _ => (false, OwnedMessage::Text(trimmed.to_string())),
-            };
-
-            stdin_sink
-                .send(msg)
-                .expect("Sending message across stdin channel.");
-
-            if close {
-                break;
-            }
-        }
-    });
-
-    let runner = ClientBuilder::new(CONNECTION)
-        .unwrap()
-        .add_protocol("rust-websocket")
-        .async_connect_insecure()
-        .and_then(|(duplex, _)| {
-            let (sink, stream) = duplex.split();
-            stream
-                .filter_map(|message| {
-                    println!("Received Message: {:?}", message);
-                    match message {
-                        OwnedMessage::Close(e) => Some(OwnedMessage::Close(e)),
-                        OwnedMessage::Ping(d) => Some(OwnedMessage::Pong(d)),
-                        _ => None,
-                    }
-                })
-                .select(stdin_ch.map_err(|_| WebSocketError::NoDataAvailable))
-                .forward(sink)
-        });
-    runtime.block_on(runner).unwrap();
 }
