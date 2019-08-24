00f6194b0280fa4e7161bf6b9f3b9ee38bc8b8f0
Jongwhan Lee <leejw51@gmail.com>
2019-08-24 23:17:46 +0800

    working

--- test_jsclient/src/main.rs
+++ test_jsclient/src/main.rs
@@ -1,23 +1,17 @@
-extern crate futures;
-extern crate tokio;
-extern crate websocket;
-
-use futures::future::Future;
-use futures::sink::Sink;
-use futures::stream::Stream;
-use futures::sync::mpsc;
-use futures::sync::mpsc::Sender;
+use std::time;
 //use jsonrpc_ws_client::connect;
+use futures::future::lazy;
 use jsonrpc_client_transports::transports::ws::connect;
-use jsonrpc_client_transports::{RawClient, TypedClient};
+use jsonrpc_client_transports::{RawClient};
 use jsonrpc_client_transports::{RpcChannel, RpcError};
-use std::io::stdin;
+use jsonrpc_core::{Error, ErrorCode, IoHandler, Params, Value};
 use std::thread;
+use tokio::runtime::Runtime;
 use websocket::result::WebSocketError;
 use websocket::{ClientBuilder, OwnedMessage};
 const CONNECTION: &'static str = "ws://localhost:26657/websocket";
 #[derive(Clone)]
-struct TestClient(TypedClient);
+struct TestClient(RawClient);
 
 impl From<RpcChannel> for TestClient {
     fn from(channel: RpcChannel) -> Self {
@@ -25,24 +19,11 @@ impl From<RpcChannel> for TestClient {
     }
 }
 
-impl TestClient {
-    fn hello(&self, msg: &'static str) -> impl Future<Item = String, Error = RpcError> {
-        self.0.call_method("hello", "String", (msg,))
-    }
-    fn fail(&self) -> impl Future<Item = (), Error = RpcError> {
-        self.0.call_method("fail", "()", ())
-    }
-}
 fn main() {
-    let client = connect::<TestClient>(CONNECTION)
-        .and_then(|stream| {
-            println!("created stream");
-
-            Ok(())
-        })
-        .map_err(|err| {
-            println!("connection error = {:?}", err);
-        });
-
-    println!("OK");
+    let mut rt = Runtime::new().unwrap();
+    let a = connect::<TestClient>(CONNECTION);
+    let b: TestClient = rt.block_on(a.unwrap()).unwrap();
+    loop {
+        thread::sleep(time::Duration::from_millis(1000));
+    }
 }
