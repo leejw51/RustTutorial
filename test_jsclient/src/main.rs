e5f3a9f9a71e4b1626b62a193e3355342f5b2582
Jongwhan Lee <leejw51@gmail.com>
2019-08-25 19:02:57 +0800

    refactoring

--- test_jsclient/src/main.rs
+++ test_jsclient/src/main.rs
@@ -1,29 +1,26 @@
 use std::time;
-//use jsonrpc_ws_client::connect;
-use futures::future::lazy;
 use jsonrpc_client_transports::transports::ws::connect;
 use jsonrpc_client_transports::RawClient;
-use jsonrpc_client_transports::{RpcChannel, RpcError};
-use jsonrpc_core::{Error, ErrorCode, IoHandler, Params, Value};
+use jsonrpc_client_transports::RpcChannel;
+use jsonrpc_core::Params;
 use std::thread;
 use tokio::runtime::Runtime;
-use websocket::result::WebSocketError;
-use websocket::{ClientBuilder, OwnedMessage};
+use websocket:: OwnedMessage;
 use serde_json::map::Map;
 const CONNECTION: &'static str = "ws://localhost:26657/websocket";
 #[derive(Clone)]
-struct TestClient(RawClient);
+struct MyClient(RawClient);
 
-impl From<RpcChannel> for TestClient {
+impl From<RpcChannel> for MyClient {
     fn from(channel: RpcChannel) -> Self {
-        TestClient(channel.into())
+        MyClient(channel.into())
     }
 }
 
 fn main() {
     let mut rt = Runtime::new().unwrap();
-    let a = connect::<TestClient>(CONNECTION);
-    let b: TestClient = rt.block_on(a.unwrap()).unwrap();
+    let a = connect::<MyClient>(CONNECTION);
+    let b: MyClient = rt.block_on(a.unwrap()).unwrap();
     let mut map = Map::new();
     //map.insert("name".to_string(), "Jeffry".into());
     let fut = b.0.call_method("status", Params::Map(map));
@@ -31,7 +28,7 @@ fn main() {
 			Ok(val) => println!("{}",serde_json::to_string_pretty(&val).unwrap()),
 			Err(err) => println!("err {:?}",err),
 		}
-
+    println!("HI");
     loop {
         thread::sleep(time::Duration::from_millis(1000));
     }
