95f96fbdeee4062d0685f637161a722d698b6678
Jongwhan Lee <jonghwan@crypto.com>
2019-08-26 15:22:04 +0800

    compile

--- test_jsclient/src/main.rs
+++ test_jsclient/src/main.rs
@@ -1,12 +1,12 @@
-use std::time;
 use jsonrpc_client_transports::transports::ws::connect;
 use jsonrpc_client_transports::RawClient;
 use jsonrpc_client_transports::RpcChannel;
-use jsonrpc_core::Params;
-use std::thread;
-use tokio::runtime::Runtime;
-use websocket:: OwnedMessage;
+use jsonrpc_core::types::params::Params;
 use serde_json::map::Map;
+use std::thread;
+use std::time;
+use tokio::runtime::Runtime;
+use websocket::OwnedMessage;
 const CONNECTION: &'static str = "ws://localhost:26657/websocket";
 #[derive(Clone)]
 struct MyClient(RawClient);
@@ -26,9 +26,9 @@ fn main() {
         let mut map = Map::new();
         let fut = b.0.call_method("status", Params::Map(map));
         match rt.block_on(fut) {
-			Ok(val) => println!("{}",serde_json::to_string_pretty(&val).unwrap()),
-			Err(err) => println!("err {:?}",err),
-		}
+            Ok(val) => println!("{}", serde_json::to_string_pretty(&val).unwrap()),
+            Err(err) => println!("err {:?}", err),
+        }
         thread::sleep(time::Duration::from_millis(1000));
     }
 }
