ee99811634197db602ac0ca05d326e8ec36cd5ab
Jongwhan Lee <jonghwan@crypto.com>
2019-08-26 18:47:02 +0800

    OK

--- test_jsclient/src/main.rs
+++ test_jsclient/src/main.rs
@@ -7,6 +7,8 @@ use std::thread;
 use std::time;
 use tokio::runtime::Runtime;
 use websocket::OwnedMessage;
+use tokio::net::tcp::TcpStream;
+use tokio::prelude::*;
 const CONNECTION: &'static str = "ws://localhost:26657/websocket";
 #[derive(Clone)]
 struct MyClient(RawClient);
@@ -21,14 +23,30 @@ fn main() {
     let mut rt = Runtime::new().unwrap();
     let a = connect::<MyClient>(CONNECTION);
     let b: MyClient = rt.block_on(a.unwrap()).unwrap();
+    let mut map = Map::new();
+    map.insert("query".to_string(), "tm.event='NewBlock'".into());
+    let c = b.0.subscribe(
+        "subscribe",
+        Params::Map(map),
+        "subscribe",
+        "unsubscribe_hello",
+    );
+    rt.block_on(c)
+    .and_then(|stream| {
+        println!("got stream!");
+        Ok(())
+    });
+
+    thread::sleep(time::Duration::from_millis(1000));
+
     //map.insert("name".to_string(), "Jeffry".into());
-    loop {
-        let mut map = Map::new();
-        let fut = b.0.call_method("status", Params::Map(map));
-        match rt.block_on(fut) {
-            Ok(val) => println!("{}", serde_json::to_string_pretty(&val).unwrap()),
-            Err(err) => println!("err {:?}", err),
-        }
-        thread::sleep(time::Duration::from_millis(1000));
-    }
+    /* loop {
+          let mut map = Map::new();
+          let fut = b.0.call_method("status", Params::Map(map));
+          match rt.block_on(fut) {
+              Ok(val) => println!("{}", serde_json::to_string_pretty(&val).unwrap()),
+              Err(err) => println!("err {:?}", err),
+          }
+          thread::sleep(time::Duration::from_millis(1000));
+    }*/
 }
