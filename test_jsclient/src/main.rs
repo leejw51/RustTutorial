87af5eaa777995abbf3b313c860750064328fea0
Jongwhan Lee <leejw51@gmail.com>
2019-08-26 22:23:19 +0800

    got stream

--- test_jsclient/src/main.rs
+++ test_jsclient/src/main.rs
@@ -24,14 +24,16 @@ fn main() {
     println!("connected..");
     let mut map = Map::new();
     map.insert("query".to_string(), "tm.event='NewBlock'".into());
-    let fut = b.0.call_method("subscribe", Params::Map(map));
-    match rt.block_on(fut) {
-        Ok(val) => println!("{}", serde_json::to_string_pretty(&val).unwrap()),
-        Err(err) => println!("err {:?}", err),
-    }
+    // let fut = b.0.call_method("subscribe", Params::Map(map));
+    //rt.block_on(fut);
+    let fut = b.0.subscribe("scribe", Params::Map(map), "scribe", "");
+    rt.block_on(fut).and_then(|stream| {
+        println!("got scream");
+        Ok(())
+    });
     loop {
-        thread::sleep(time::Duration::from_millis(1000));
         println!("polling");
+        thread::sleep(time::Duration::from_millis(1000));
     }
     println!("done");
 }
