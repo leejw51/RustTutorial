ef1a52ae2a3e740f563c47bfb9620b86bca231af
Jongwhan Lee <jonghwan@crypto.com>
2019-08-27 10:35:05 +0800

    working

--- test_jsclient/src/main.rs
+++ test_jsclient/src/main.rs
@@ -1,3 +1,5 @@
+use futures::future::Future;
+use futures::stream::Stream;
 use jsonrpc_client_transports::transports::ws::connect;
 use jsonrpc_client_transports::RawClient;
 use jsonrpc_client_transports::RpcChannel;
@@ -7,6 +9,10 @@ use std::thread;
 use std::time;
 use tokio::runtime::Runtime;
 use websocket::OwnedMessage;
+
+use tokio::io;
+use tokio::net::TcpStream;
+
 const CONNECTION: &'static str = "ws://localhost:26657/websocket";
 #[derive(Clone)]
 struct MyClient(RawClient);
@@ -26,14 +32,29 @@ fn main() {
     map.insert("query".to_string(), "tm.event='NewBlock'".into());
     // let fut = b.0.call_method("subscribe", Params::Map(map));
     //rt.block_on(fut);
-    let fut = b.0.subscribe("scribe", Params::Map(map), "scribe", "");
-    rt.block_on(fut).and_then(|stream| {
-        println!("got scream");
-        Ok(())
-    });
-    loop {
-        println!("polling");
-        thread::sleep(time::Duration::from_millis(1000));
-    }
-    println!("done");
+    let fut =
+        b.0.subscribe("subscribe", Params::Map(map), "subscribe", "");
+    let stream = rt.block_on(fut).unwrap();
+    let fut2 = stream.into_future();
+    let fut3 = fut2.map(|a| {});
+    rt.block_on(fut3);
+}
+
+fn main2() {
+    let addr = "127.0.0.1:26657".parse().unwrap();
+
+    let future = TcpStream::connect(&addr)
+        .and_then(|socket| io::write_all(socket, b"status"))
+        .and_then(|(socket, _)| {
+            println!("read bytes");
+            // read exactly 11 bytes
+            io::read_exact(socket, vec![0; 20])
+        })
+        .and_then(|(socket, buf)| {
+            println!("got {:?}", buf);
+            Ok(())
+        })
+        .map_err(|_| println!("failed"));
+
+    tokio::run(future);
 }
