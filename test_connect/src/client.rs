6624ec82d0be0067966b48c82ac53ff98f23f65f
Jongwhan Lee <leejw51@gmail.com>
2019-09-10 00:38:41 +0800

    client

--- test_connect/src/client.rs
+++ test_connect/src/client.rs
@@ -1,3 +1,24 @@
+use tokio::io;
+use tokio::net::TcpStream;
+use tokio::prelude::*;
 pub fn client() {
     quest::ask("client==========================================\n");
+    let addr = "127.0.0.1:8080".parse().unwrap();
+    let client = TcpStream::connect(&addr)
+        .and_then(|stream| {
+            println!("created stream");
+
+            io::write_all(stream, "hello world\n").then(|result| {
+                println!("wrote to stream; success={:?}", result.is_ok());
+                Ok(())
+            })
+        })
+        .map_err(|err| {
+            // All tasks must have an `Error` type of `()`. This forces error
+            // handling and helps avoid silencing failures.
+            //
+            // In our example, we are only going to log the error to STDOUT.
+            println!("connection error = {:?}", err);
+        });
+    tokio::run(client);
 }
--- test_connect/src/server.rs
+++ test_connect/src/server.rs
@@ -1,3 +1,39 @@
-pub fn server() {
-    quest::ask("server===================================\n");
+#![deny(warnings)]
+
+extern crate tokio;
+
+use tokio::io;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+
+use std::env;
+use std::net::SocketAddr;
+
+pub fn server() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
+    let addr = addr.parse::<SocketAddr>()?;
+
+    let socket = TcpListener::bind(&addr)?;
+    println!("Listening on: {}", addr);
+
+    let done = socket
+        .incoming()
+        .map_err(|e| println!("failed to accept socket; error = {:?}", e))
+        .for_each(move |socket| {
+            let (reader, writer) = socket.split();
+            let amt = io::copy(reader, writer);
+            let msg = amt.then(move |result| {
+                match result {
+                    Ok((amt, _, _)) => println!("wrote {} bytes", amt),
+                    Err(e) => println!("error: {}", e),
+                }
+
+                Ok(())
+            });
+
+            tokio::spawn(msg)
+        });
+
+    tokio::run(done);
+    Ok(())
 }
