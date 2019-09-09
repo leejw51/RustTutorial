c382922ba65e07541e9a04245ba3ca4c6cbf1bcb
Jongwhan Lee <leejw51@gmail.com>
2019-09-09 23:59:16 +0800

    ok

--- test_connect/src/main.rs
+++ test_connect/src/main.rs
@@ -1,3 +1,27 @@
+mod client;
+mod server;
+
+use quest::{ask, text, yesno};
+use crate::server::server;
+use crate::client::client;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        ask("1. server\n");
+        ask("2. client\n");
+        ask("q. exit\n");
+        let a = text().unwrap();
+        match a.as_str() {
+            "1" => {
+                server();
+            }
+            "2" => {
+                client();
+            }
+            "q" => {
+                break;
+            }
+            _ => {}
+        }
+    }
 }
