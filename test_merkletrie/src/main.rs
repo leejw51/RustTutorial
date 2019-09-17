89693a5f64fa1c19eba8ffff1820130b2fe05d8a
Jongwhan Lee <jonghwan@crypto.com>
2019-09-17 18:35:03 +0800

    db

--- test_merkletrie/src/main.rs
+++ test_merkletrie/src/main.rs
@@ -1,3 +1,11 @@
+use sled::Db;
+
+fn run() -> Result<(), ()> {
+    let path = ".db";
+    let _tree = Db::open(path).map_err(|_e| {})?;
+    Ok(())
+}
 fn main() {
+    run().expect("OK");
     println!("Hello, world!");
 }
