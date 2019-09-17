e938f35dedb3bf6d5833b7dc75749db846de039e
Jongwhan Lee <leejw51@gmail.com>
2019-09-17 21:35:12 +0800

    windows electron works

--- neon_electron/hello-world/native/build.rs
+++ neon_electron/hello-world/native/build.rs
@@ -1,7 +1,12 @@
 use neon_build;
-
+extern crate cc;
 fn main() {
     neon_build::setup(); // must be called in build.rs
 
     // add project-specific build logic here...
+     cc::Build::new()
+        .cpp(true)
+        .static_crt(true)
+        .file("src/win_delay_load_hook.cc")
+        .compile("hook");
 }
