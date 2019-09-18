4c36ebed50538c2f6a0adbaedc68a223cbc10dd1
Jongwhan Lee <leejw51@gmail.com>
2019-09-19 00:32:28 +0800

    ok

--- test_react/src/App.js
+++ test_react/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Paper>
+        <Typography variant='h5' component='h3'>
+          This is a sheet of paper.
+        </Typography>
+        <Typography component='p'>
+          Paper can be used to build surface or other elements for your
+          application.
+        </Typography>
+        <TextField id='standard-name' label='Name' margin='normal' /> <br />
+        <Button variant='contained' color='primary'>
+          Hello, hello Can you hear me, as I scream your name Hello, hello Do
+          you need me, before I fade away
+        </Button>
+      </Paper>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
