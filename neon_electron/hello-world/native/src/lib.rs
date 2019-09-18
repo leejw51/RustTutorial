999e21cc08de4bc433c7ac13f1d98ff5152f0669
Jongwhan Lee <jonghwan@crypto.com>
2019-09-18 10:29:26 +0800

    works

--- neon_electron/hello-world/native/src/lib.rs
+++ neon_electron/hello-world/native/src/lib.rs
@@ -2,7 +2,7 @@ use neon::prelude::*;
 use neon::register_module;
 
 fn hello_world(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello world!"))
+    Ok(cx.string("hello world2!"))
 }
 
 register_module!(mut m, { m.export_function("helloWorld", hello_world) });
