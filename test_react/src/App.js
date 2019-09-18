a6898cc5dab66ef57e71d5e381ae1cfc8dad1830
Jongwhan Lee <leejw51@gmail.com>
2019-09-19 00:46:28 +0800

    react

--- test_react/src/App.js
+++ test_react/src/App.js
@@ -5,9 +5,53 @@ import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
-function App () {
-  return (
-    <div>
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+
+function createData (name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein }
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9)
+]
+
+
+
+
+class  App  extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+    this.index=1;
+
+    // 콜백에서 `this`가 작동하려면 아래와 같이 바인딩 해주어야 합니다.
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+ 
+  handleClick() {
+    console.log("click2");
+    var a=createData('Gingerbread'+this.index, 356, 16.0, 49, 3.9);
+    rows.push(a);
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+    this.index++;
+  }
+
+  render() {
+    return (
+      <div>
       <Paper>
         <Typography variant='h5' component='h3'>
           This is a sheet of paper.
@@ -17,13 +61,45 @@ function App () {
           application.
         </Typography>
         <TextField id='standard-name' label='Name' margin='normal' /> <br />
-        <Button variant='contained' color='primary'>
-          Hello, hello Can you hear me, as I scream your name Hello, hello Do
-          you need me, before I fade away
+
+
+        <Button variant='contained' color='primary' onClick={this.handleClick}>
+         Add Row
         </Button>
+
+
+        <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
       </Paper>
     </div>
-  )
+    );
+  }
+
+
+ 
 }
 
 export default App
