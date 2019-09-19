b4e735af530885f70bb96d39afe0a8e6e799591c
Jongwhan Lee <jonghwan@crypto.com>
2019-09-19 17:29:35 +0800

    secp256k1

--- test_hdwallet/src/main.rs
+++ test_hdwallet/src/main.rs
@@ -1,8 +1,11 @@
 use bip39::{Language, Mnemonic, Seed};
-use ethsign::SecretKey;
+
 use std::str::FromStr;
 use tiny_hderive::bip32::ExtendedPrivKey;
 use tiny_hderive::bip44::ChildNumber;
+
+use secp256k1::{Message, PublicKey, Secp256k1, SecretKey};
+
 fn main() {
     let phrase = "panda eyebrow bullet gorilla call smoke muffin taste mesh discover soft ostrich alcohol speed nation flash devote level hobby quick inner drive ghost inside";
     let mnemonic = Mnemonic::from_phrase(phrase, Language::English).unwrap();
@@ -20,18 +23,22 @@ fn main() {
         let account =
             ExtendedPrivKey::derive(seed.as_bytes(), format!("m/44'/60'/0'/0/{}", i).as_str())
                 .unwrap();
-        let secret_key = account.secret();
-        let secret_key2 = SecretKey::from_raw(&secret_key).unwrap();
-        let public_key = secret_key2.public();
+        let secret_key_bytes = account.secret();
+        let secp = Secp256k1::new();
+        let secret_key =
+            SecretKey::from_slice(&secret_key_bytes).expect("32 bytes, within curve order");
+        let public_key = PublicKey::from_secret_key(&secp, &secret_key);
+        let message = Message::from_slice(&[0xab; 32]).expect("32 bytes");
+        let sig = secp.sign(&message, &secret_key);
+        assert!(secp.verify(&message, &sig, &public_key).is_ok());
+
         println!(
-            "({})  private_key={} length={} \npublic_key={} length={}\naddress={}  length={}",
+            "({})  private_key={}/{} public_key={}/{}",
             i,
-            hex::encode(secret_key),
+            &secret_key.to_string()[..8],
             secret_key.len(),
-            hex::encode(public_key.bytes().to_vec()),
-            public_key.bytes().len(),
-            hex::encode(public_key.address().to_vec()),
-            public_key.address().len(),
+            &public_key.to_string()[..8],
+            public_key.to_string().len() / 2,
         );
     }
 }
