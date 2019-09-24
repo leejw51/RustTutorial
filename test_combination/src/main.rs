45f94c50984d1bda6fd10f2feb1d0fc79a56e6cb
Jongwhan Lee <jonghwan@crypto.com>
2019-09-24 16:16:23 +0800

    OK

--- test_combination/src/main.rs
+++ test_combination/src/main.rs
@@ -1,3 +1,16 @@
+use itertools::Itertools;
 fn main() {
-    println!("Hello, world!");
+    let numbers = vec![10, 50, 8, 9, 200, 1000, 700];
+    println!("numbers={:?}", numbers);
+    let it: Vec<Vec<i32>> = numbers
+        .into_iter()
+        .combinations(3)
+        .map(|mut combination| {
+            combination.sort();
+            combination
+        })
+        .collect();
+    for i in it {
+        println!("{:?}", i);
+    }
 }
