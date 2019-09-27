d3e15d9b314cc6f76d9bedf1bef6db8adfb6bce2
Jongwhan Lee <jonghwan@crypto.com>
2019-09-27 16:10:07 +0800

    OK

--- test_unittest/src/main.rs
+++ test_unittest/src/main.rs
@@ -1,5 +1,45 @@
+#[derive(Clone, Debug)]
+struct Apple {
+    value: i32,
+    kind: String,
+}
+
+trait Product {
+    fn buy(&self, value: i32) -> bool {
+        false
+    }
+}
+
+trait Food {
+    fn eat(&self) {
+        println!("eat");
+    }
+}
+impl Product for Apple {
+    fn buy(&self, value: i32) -> bool {
+        self.eat();
+        if value > self.value {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+impl Food for Apple {
+    fn eat(&self) {
+        println!("eat apple {}", self.kind);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    let a = Apple {
+        value: 2000,
+        kind: "mac".into(),
+    };
+    let b = a.buy(3000);
+    println!("buy= {}", b);
+    a.eat();
 }
 
 #[cfg(test)]
