4cba3ae63f744bf5e1f9e3ed291ecf66e3b891ae
Jongwhan Lee <jonghwan@crypto.com>
2019-10-02 11:02:14 +0800

    show time

--- test_trait/src/main.rs
+++ test_trait/src/main.rs
@@ -1,3 +1,19 @@
+struct Program {}
+impl Program {
+    pub fn new() -> Self {
+        Program {}
+    }
+    pub fn run(&self) {
+        loop {
+            std::thread::sleep(std::time::Duration::from_millis(1000));
+            println!("running {}", chrono::Local::now());
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let p = Program::new();
+    let handle = std::thread::spawn(move || {
+        p.run();
+    });
+    handle.join();
 }
