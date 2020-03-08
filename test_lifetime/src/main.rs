ff85e00ef412ed0b662609132bec5a98f4386dca
jongwhan lee <jonghwan@crypto.com>
2020-03-08 20:24:08 +0800

    lifetime

--- test_lifetime/src/main.rs
+++ test_lifetime/src/main.rs
@@ -3,8 +3,24 @@ fn skip_prefix<'a>(line: &'a str, prefix: &'a str) -> &'a str {
 }
 
 
+struct Foo<'a> {
+    x: &'a i32,
+}
 
-fn main() {
+impl<'a> Foo<'a> {
+    fn x(&self) -> &'a i32 { self.x }
+}
+
+fn test2() {
+    let y = &5; // This is the same as `let _y = 5; let y = &_y;`.
+    let f = Foo { x: y };
+
+    println!("x is: {}", f.x());
+}
+
+
+
+fn test1() {
     println!("Hello, world!");
     let line = "lang:en=Hello World!";
     let lang = "en";
@@ -15,4 +31,9 @@ fn main() {
         v = skip_prefix(line, p.as_str());  //  |
         println!("{}",v);
     }
+}
+
+
+fn main() {
+    test2();
 }
