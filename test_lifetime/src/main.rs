445eb59c2f63e861157685e4bf75f802622861e9
jongwhan lee <jonghwan@crypto.com>
2020-03-08 20:13:59 +0800

    OK

--- test_lifetime/src/main.rs
+++ test_lifetime/src/main.rs
@@ -1,3 +1,18 @@
+fn skip_prefix<'a>(line: &'a str, prefix: &'a str) -> &'a str {
+    line
+}
+
+
+
 fn main() {
     println!("Hello, world!");
+    let line = "lang:en=Hello World!";
+    let lang = "en";
+
+    let v;
+    {
+        let p = format!("lang:{}=", lang);  // -+ `p` comes into scope.
+        v = skip_prefix(line, p.as_str());  //  |
+        println!("{}",v);
+    }
 }
