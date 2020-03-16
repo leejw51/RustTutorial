4060a76af86b95bbe1808a060c24184fbca98f2c
jongwhan lee <jonghwan@crypto.com>
2020-03-16 22:47:21 +0800

    test:

--- test_db/src/main.rs
+++ test_db/src/main.rs
@@ -1,5 +1,5 @@
 use blake2::{Blake2b, Blake2s, Digest};
-
+use rocksdb::{DB, Options};
 fn test_sled()-> Result<(), String> {
     let tree = sled::open("./sled_storage").map_err(|e| e.to_string())?;
     let mut i: u64=0;
@@ -23,8 +23,33 @@ fn test_sled()-> Result<(), String> {
     
     Ok(())
 }
+
+fn test_rocksdb()-> Result<(), String> {
+    let db =  DB::open_default("./rocksdb_storage")?;
+    let mut i: u64=0;
+    loop 
+    {
+        let mut hasher = Blake2s::new();
+        let key= format!("{}",i);
+        hasher.input(key.as_bytes());
+
+        let mut hasher_data= Blake2b::new();
+        let data= format!("data {}",i*100);
+        hasher_data.input(data.as_bytes());
+
+        let key_res = hasher.result();
+        let data_res = hasher_data.result();
+        println!("{} {} {}",i,hex::encode(&key_res[..]), hex::encode(&data_res[..]));
+        db.put(&key_res[..], &data_res[..]);
+        i=i+1;
+        println!("i={}",i);
+    }
+    
+    Ok(())
+}
 fn main() {
    
     println!("Hello, world!");
+   // test_rocksdb();
     test_sled();
 }
