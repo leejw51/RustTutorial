f91d7bf027d3371b6250061816e48093f02f6c3c
Jongwhan Lee <leejw51@gmail.com>
2020-03-24 22:59:08 +0800

    add writing

--- test_sled/src/main.rs
+++ test_sled/src/main.rs
@@ -1,21 +1,29 @@
 use std::str;
+use blake2::{Blake2b, Blake2s, Digest};
+use std::time::Instant;
+mod test;
 
-fn test_db() -> Result<(), failure::Error> {
+fn write_db(count:i32) -> Result<(), failure::Error> {
     let tree = sled::open("./disk")?;
-    // set and get
-    tree.insert(b"ok", b"apple".to_vec())?;
-    tree.insert(b"ok", b"apple5".to_vec())?;
-    println!(
-        "{:?}",
-        str::from_utf8(&tree.get(b"ok").unwrap_or_default().unwrap_or_default())
-            .unwrap_or_default()
-    );
-    if let Some(found) = tree.get(b"ok")? {
-        println!("{}", str::from_utf8(&found)?);
+    let start= Instant::now();
+    for i in 0..count {
+        let mut hasher = Blake2s::new();    
+        hasher.input(format!("{}",i));
+        let res= hasher.result();
+
+        let mut hasher_value = Blake2s::new();    
+        hasher_value.input(format!("data {}",i));
+        let res_value= hasher_value.result();
+        println!("{} {}", hex::encode(res), hex::encode(res_value));
+        tree.insert(res.as_slice(), res_value.as_slice())?;
     }
+    
     tree.flush()?;
+    println!("time={:?}", start.elapsed());
     Ok(())
 }
+
+
 fn main() {
-    test_db().unwrap();
+    write_db(1000).unwrap();
 }
