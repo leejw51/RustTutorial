5acd356d6b1972875fc2e9c9c8e65186bcfc348c
Jongwhan Lee <leejw51@gmail.com>
2020-03-24 23:53:50 +0800

    speed check

--- test_sled/src/main.rs
+++ test_sled/src/main.rs
@@ -1,7 +1,16 @@
 mod test;
 mod sled_test;
+mod rocks;
 fn main() {
-    let count=1000;
+    let count=1000000;
+    println!("writing {} items", count);
+    print!("sled normal '");
     sled_test::write_db(count).unwrap();
+    print!("sled batch '");
     sled_test::write_db_batch(count).unwrap();
+    print!("rocks normal '");
+    rocks::write_db(count).unwrap();
+    print!("rocks batch '");
+    rocks::write_db_batch(count).unwrap();
 }
+
