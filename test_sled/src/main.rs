f51c78a4c559ed78e052e7437a9e2629fdffccf0
Jongwhan Lee <leejw51@gmail.com>
2020-03-24 22:33:01 +0800

    db

--- test_sled/src/main.rs
+++ test_sled/src/main.rs
@@ -1,26 +1,21 @@
-use sled::Db;
 use std::str;
-fn main() {
-    let tree = Db::open("./disk").unwrap();
 
+fn test_db() -> Result<(), failure::Error> {
+    let tree = sled::open("./disk")?;
     // set and get
-    tree.insert(b"ok", b"apple".to_vec());
-    tree.insert(b"ok", b"apple5".to_vec());
-    println!("{:?}", str::from_utf8(&tree.get(b"ok").unwrap_or_default().unwrap_or_default()).unwrap_or_default());
-    match tree.get(b"ok") {
-        Ok(a) => {
-            match a {
-                Some(c) => {
-                    let r= str::from_utf8(&c).unwrap();
-                    println!("{:?}",r);
-
-                },
-                _=>{}
-
-            }
-        },
-        _ => {},
+    tree.insert(b"ok", b"apple".to_vec())?;
+    tree.insert(b"ok", b"apple5".to_vec())?;
+    println!(
+        "{:?}",
+        str::from_utf8(&tree.get(b"ok").unwrap_or_default().unwrap_or_default())
+            .unwrap_or_default()
+    );
+    if let Some(found) = tree.get(b"ok")? {
+        println!("{}", str::from_utf8(&found)?);
     }
-        // block until all operations are on-disk
-    tree.flush();
+    tree.flush()?;
+    Ok(())
+}
+fn main() {
+    test_db().unwrap();
 }
