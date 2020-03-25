39db5184618c72e9bfa39a0cc718028e7c7dd062
jongwhan lee <jonghwan@crypto.com>
2020-03-25 20:55:43 +0800

    ok

--- test_enum/target/rls/debug/deps/test_enum-4f327f0f58f045f1.d
+++
@@ -1,5 +0,0 @@
-/home/leejw51/Github/RustTutorial/test_enum/target/rls/debug/deps/test_enum-4f327f0f58f045f1.rmeta: src/main.rs
-
-/home/leejw51/Github/RustTutorial/test_enum/target/rls/debug/deps/test_enum-4f327f0f58f045f1.d: src/main.rs
-
-src/main.rs:
