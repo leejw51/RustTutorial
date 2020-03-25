83e95567b91ba0528ea6404dba1b82abd7787850
jongwhan lee <jonghwan@crypto.com>
2020-03-26 00:10:20 +0800

    ok

--- test_jsonrpc/src/main.rs
+++ test_jsonrpc/src/main.rs
@@ -1,3 +1,15 @@
+use jsonrpc_core::*;
+use jsonrpc_http_server::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut io = IoHandler::new();
+    io.add_method("say_hello", |_: Params| {
+        Ok(Value::String("hello".to_string()))
+    });
+
+    let _server = ServerBuilder::new(io)
+        .start_http(&"127.0.0.1:3030".parse().unwrap())
+        .expect("Unable to start RPC server");
+
+    _server.wait();
 }
