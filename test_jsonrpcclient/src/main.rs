38f721a982da8f669a3a7e87f608a1f665cd3c1c
jongwhan lee <jonghwan@crypto.com>
2020-03-29 17:54:57 +0800

    works

--- test_jsonrpcclient/src/main.rs
+++ test_jsonrpcclient/src/main.rs
@@ -1,3 +1,14 @@
+use jsonrpc::client::Client ;
+use jsonrpc::Request;
+use serde_json::{json, Value};
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client::new("http://localhost:26657".to_owned(), None, None);
+    let name="status";
+    let params: Vec<Value>= vec![];
+    let request = client.build_request(name, &params);
+    println!("{:?}", request);
+    let response = client.send_request(&request).unwrap();    
+    println!("{:?}", response.result);
+
 }
