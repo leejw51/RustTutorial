33a216d828f7e600fbaeed49b34ea0c585823bcc
jongwhan lee <jonghwan@crypto.com>
2020-04-18 18:49:38 +0800

    clone

--- test_clone/src/main.rs
+++ test_clone/src/main.rs
@@ -1,3 +1,40 @@
+trait Fruit: Send + Sync + Clone {
+    fn eat(&self);
+}
+
+#[derive(Clone, Default, Debug)]
+struct Apple {
+    price: i32,
+}
+
+impl Apple {}
+
+impl Fruit for Apple {
+    fn eat(&self) {
+        println!("eat apple {}", self.price);
+    }
+}
+
+struct Store<T>
+where
+    T: Fruit,
+{
+    fruit: T,
+}
+
+impl<T> Store<T>
+where
+    T: Fruit,
+{
+    fn eat(&self) {
+        self.fruit.eat();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let apple = Apple { price: 100 };
+    let store = Store {
+        fruit: apple.clone(),
+    };
+    store.eat();
 }
