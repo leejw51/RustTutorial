faa6b035af588fee7eb409611c2caaf30a329a6c
jongwhan lee <jonghwan@crypto.com>
2020-04-19 17:59:11 +0800

    simple

--- test_clone/src/main.rs
+++ test_clone/src/main.rs
@@ -1,7 +1,77 @@
-mod test1;
-mod test2;
+trait DiskInterface {
+    fn boot(&self);
+}
+#[derive(Default)]
+struct Disk {}
+impl DiskInterface for Disk {
+    fn boot(&self) {
+        println!("booting disk");
+    }
+}
 
-fn main() {
-    //test1::main();
-    // test2::main();
+trait WebsocketInterface {}
+
+#[derive(Default)]
+struct Websocket {}
+impl WebsocketInterface for Websocket {}
+
+trait BuilderInterface {}
+#[derive(Default)]
+struct Builder {}
+impl BuilderInterface for Builder {}
+
+#[derive(Default)]
+struct Client<D, W, B>
+where
+    D: DiskInterface,
+    W: WebsocketInterface,
+    B: BuilderInterface,
+{
+    pub disk: Box<D>,
+    pub socket: Box<W>,
+    pub builder: Vec<B>,
+}
+
+#[derive(Default)]
+struct Client2<D, W, B>
+where
+    D: DiskInterface,
+    W: WebsocketInterface,
+    B: BuilderInterface,
+{
+    pub disk: D,
+    pub socket: W,
+    pub builder: B,
+    pub client: Client<D, W, B>,
+}
+
+type MyClient2 = Client2<Disk, Websocket, Builder>;
+
+#[derive(Default)]
+struct Program {
+    client2: MyClient2,
+}
+
+impl Program {
+    pub fn initialize(&mut self) {
+        let client = Client {
+            disk: Box::new(Disk {}),
+            socket: Box::new(Websocket {}),
+            builder: Vec::<Builder>::new(),
+        };
+        let client2 = Client2 {
+            disk: Disk {},
+            socket: Websocket {},
+            builder: Builder {},
+            client,
+        };
+        self.client2 = client2;
+    }
+    // add code here
+}
+
+pub fn main() {
+    println!("OK");
+    let mut p = Program::default();
+    p.initialize();
 }
