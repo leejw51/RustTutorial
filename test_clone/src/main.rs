152a036c3cf74aeb5cd7ed57e7cb679989d6fefd
jongwhan lee <jonghwan@crypto.com>
2020-04-19 17:04:21 +0800

    OK

--- test_clone/src/main.rs
+++ test_clone/src/main.rs
@@ -1,49 +1,5 @@
-trait Food: Send + Sync {
-    fn eat(&self);
-}
-
-#[derive(Clone, Default, Debug)]
-struct Apple {
-    price: i32,
-}
-
-impl Apple {}
-
-impl Food for Apple {
-    fn eat(&self) {
-        println!("eat apple {}", self.price);
-    }
-}
-
-#[derive(Clone, Default, Debug)]
-struct Udon {
-    price: i32,
-}
-impl Udon {}
-
-impl Food for Udon {
-    fn eat(&self) {
-        println!("eat udon {}", self.price);
-    }
-}
-
-type MyFood = Box<Food>;
-#[derive(Default)]
-struct Store {
-    pub foods: Vec<MyFood>,
-}
-
-impl Store {
-    fn eat(&self) {
-        for a in &self.foods {
-            a.eat();
-        }
-    }
-}
+mod test1;
 
 fn main() {
-    let mut store = Store::default();
-    store.foods.push(Box::new(Apple { price: 2 }));
-    store.foods.push(Box::new(Udon { price: 3 }));
-    store.eat();
+    println!("OK");
 }
