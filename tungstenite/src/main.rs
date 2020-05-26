377a7ff04d67a3151cce5d50dfe0e6c8576a40bd
lee jongwhan <leejw51@gmail.com>
2020-05-26 20:17:32 +0800

    OK2

--- tungstenite/src/main.rs
+++ tungstenite/src/main.rs
@@ -25,9 +25,6 @@ async fn main() {
 
     let url = url::Url::parse(&connect_addr).unwrap();
 
-    //  let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
-    // tokio::spawn(read_stdin(stdin_tx));
-
     let (mut ws_stream, _) = connect_async(url).await.expect("Failed to connect");
     println!("WebSocket handshake has been successfully completed");
     let closemsg = CloseFrame {
@@ -35,19 +32,6 @@ async fn main() {
         reason: std::borrow::Cow::Borrowed("OK"),
     };
     ws_stream.close(Some(closemsg)).await;
-
-    /*let (write, read) = ws_stream.split();
-
-    let stdin_to_ws = stdin_rx.map(Ok).forward(write);
-    let ws_to_stdout = {
-        read.for_each(|message| async {
-            let data = message.unwrap().into_data();
-            tokio::io::stdout().write_all(&data).await.unwrap();
-        })
-    };*/
-
-    //  pin_mut!(stdin_to_ws, ws_to_stdout);
-    // future::select(stdin_to_ws, ws_to_stdout).await;
 }
 
 // Our helper method which will read data from stdin and send it along the
