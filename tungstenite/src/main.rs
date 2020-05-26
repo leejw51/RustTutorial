f7a88fe612d22f81bdb05bcfbf0892e91525df79
lee jongwhan <leejw51@gmail.com>
2020-05-26 20:18:53 +0800

    OK3

--- tungstenite/src/main.rs
+++ tungstenite/src/main.rs
@@ -33,18 +33,3 @@ async fn main() {
     };
     ws_stream.close(Some(closemsg)).await;
 }
-
-// Our helper method which will read data from stdin and send it along the
-// sender provided.
-async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
-    let mut stdin = tokio::io::stdin();
-    loop {
-        let mut buf = vec![0; 1024];
-        let n = match stdin.read(&mut buf).await {
-            Err(_) | Ok(0) => break,
-            Ok(n) => n,
-        };
-        buf.truncate(n);
-        tx.unbounded_send(Message::binary(buf)).unwrap();
-    }
-}
