1ac076f0989b01d40ad0dd2cdfb150d916e60c76
jongwhan lee <jonghwan@crypto.com>
2020-07-15 15:40:55 +0800

    OK

--- test_callback2/src/main.rs
+++ test_callback2/src/main.rs
@@ -1,10 +1,24 @@
-fn check(number: i32, callback: fn(i32) -> bool) {
+fn check(number: i32, callback: Box< dyn Fn(i32) -> bool>) {
     for i in 0..number {
         if callback(i) {
             println!("{}", i);
         }
     }
 }
+
+
+#[derive(Debug,Clone)]
+struct Test {
+    value: i32,
+}
 fn main() {
-    check(10, |a| a % 2 == 0);
+  
+    let mut test=Test{ value:0};
+   
+    check(10,  Box::new(move |a:i32| 
+        { 
+            let b=test.value;
+         
+            a % 2 == 0
+        }));
 }
