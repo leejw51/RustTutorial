a1ca9122b25fd0f52acc191f502239b0b85282f6
jongwhan lee <jonghwan@crypto.com>
2020-07-16 01:27:51 +0800

    OK5

--- test_callback2/src/main.rs
+++ test_callback2/src/main.rs
@@ -19,7 +19,7 @@ trait Person {
     
   }
   
-trait Storage{
+trait Storage : Sized{
     fn write(&self);
 
 }
@@ -40,16 +40,35 @@ impl Storage for DB
 
 
 
+trait MyStorage
+{
+    fn write(&self);
+}
+
+struct MyDB<T:Storage> {
+    storage:T
+}
+
+impl<T> MyStorage for MyDB<T> where T: Storage
+{
+    fn write(&self)
+    {
+        println!("write2");
+        self.storage.write();
+    }
+}
+
 #[derive(Clone)]
 struct Note {
-    storage:  Option<Arc<Mutex<dyn Storage>>>,
+    storage:  Option<Arc<Mutex<dyn MyStorage>>>,
 
 }
 fn main()
 {
     println!("storage");
     let db=DB{};
+    let mydb= MyDB{storage:db};
   
-     let mut note = Note{storage:Some(Arc::new(Mutex::new(db)))};
+     let mut note = Note{storage:Some(Arc::new(Mutex::new(mydb)))};
     note.storage.map(|s| s.lock().unwrap().write());
 }
