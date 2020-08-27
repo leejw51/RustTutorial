db6eaf644296fdbd954e6595d88c5273fa220f37
jongwhan <jonghwan@crypto.com>
2020-08-27 10:58:31 +0800

    ok

--- test_closure/src/main.rs
+++ test_closure/src/main.rs
@@ -1,4 +1,30 @@
+type MyClosure= FnMut(i64)->bool;
+struct Fruit {
+    info: String,
+    myclosure: Box<MyClosure>,
+}
+
+impl Fruit {
+    pub fn  eat(&self) {
+        println!("eat fruit");
+    }
+    pub fn run(&mut self) {
+        for i in 0..10 {
+            (*self.myclosure)(i);
+        }
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let mut count:i64 =10;
+    let closure1= move | number: i64| -> bool {
+        count+= number;
+        println!("closure1 count {}", count);
+        true
+    };
+    let mut a= Fruit {
+        info:"0".into(),
+        myclosure: Box::new(closure1),
+    };
+    a.run();
 }
