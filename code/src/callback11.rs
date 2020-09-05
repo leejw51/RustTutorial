586903d70fd641b3e15276922fdc4e49e8e061ce
jongwhan <leejw51@gmail.com>
2020-09-05 16:38:24 +0800

    closure

--- code/src/callback11.rs
+++ code/src/callback11.rs
@@ -1,14 +1,26 @@
-#[derive(Debug, Default)]
+type Mycallback = Box<dyn Fn(i64) -> i64>;
+
+#[derive(Default)]
 struct Note {
     info: String,
+    mycallback: Option<Mycallback>,
 }
 
 impl Note {
     pub fn run(&mut self) {
-        println!("run");
+        for id in 0..5 {
+            let output=self.mycallback.as_ref().unwrap()(id);
+        println!("run {} output {}",id, output);
+        }
     }
 }
 pub fn main() {
-    let mut note = Note::default();
+    let  value=1000;
+    let mut note = Note {
+        info: "".into(),
+        mycallback: None,
+    };
+    note.mycallback = Some(Box::new(move |a: i64| -> i64 { 
+        a * value }));
     note.run();
 }
