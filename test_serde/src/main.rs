6c5a4f7fb7a9a3479a38efdedbbc7184ea9157eb
a <b>
2020-12-09 20:04:08 +0800

    run

--- test_serde/src/main.rs
+++ test_serde/src/main.rs
@@ -1,3 +1,18 @@
+use serde::{Serialize,Deserialize};
+
+#[derive(Serialize,Deserialize,Default,Debug)]
+struct Point {
+  x:i32,
+  y:i32,
+  name:String,
+}
+
 fn main() {
-    println!("Hello, world!");
+  let point = Point::default();
+  let m = serde_json::to_string(&point).unwrap();
+  let m2: Point = serde_json::from_str(&m).unwrap();
+  let m3 = serde_json::to_string(&m2).unwrap();
+  println!("{}", m);
+  println!("{:?}", m2);
+  println!("{}", m3);
 }
