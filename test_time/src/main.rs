eff8aa8888e675275c61c5769fb295760da206c9
jongwhan <leejw51@gmail.com>
2021-10-16 21:14:34 +0900

    test time

---
+++ test_time/src/main.rs
@@ -0,0 +1,18 @@
+use std::time::{Duration, SystemTime};
+use std::thread::sleep;
+use chrono::prelude::*;
+use chrono::offset::LocalResult;
+
+fn main() {
+   let now = chrono::Utc::now();
+   
+   let a = now.timestamp_nanos();
+   let a1= a/(1_000_000_000);
+   let a2:u32= (a%(1_000_000_000 as i64)) as u32;
+   let b: DateTime<Utc> = DateTime::<Utc>::from_utc(
+       NaiveDateTime::from_timestamp(a1, a2), Utc);
+   println!("now {:?}", a);
+   println!("now {:?} {:?}", b, now);
+   assert!(b==now);
+   
+}
