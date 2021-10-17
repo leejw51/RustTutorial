7127ae53032939f810271c3efad925201b69b951
jongwhan <leejw51@gmail.com>
2021-10-17 15:56:53 +0900

    sqlite

--- test_sqlite/src/main.rs
+++ test_sqlite/src/main.rs
@@ -1,44 +1,49 @@
-use rusqlite::{params, Connection, Result};
+use serde::{Deserialize, Serialize};
+use serde_rusqlite::*;
 
-#[derive(Debug)]
-struct Person {
-    id: i32,
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct Example {
+    id: i64,
     name: String,
-    data: Option<Vec<u8>>,
 }
 
-fn main() -> Result<()> {
-    let conn = Connection::open_in_memory()?;
+fn test() -> std::result::Result<(), failure::Error> {
+    let connection = rusqlite::Connection::open_in_memory().unwrap();
+    connection.execute("CREATE TABLE example (id INT, name TEXT)", [])?;
 
-    conn.execute(
-        "CREATE TABLE person (
-                  id              INTEGER PRIMARY KEY,
-                  name            TEXT NOT NULL,
-                  data            BLOB
-                  )",
-        [],
-    )?;
-    let me = Person {
-        id: 0,
-        name: "Steven".to_string(),
-        data: None,
-    };
-    conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        params![me.name, me.data],
-    )?;
-
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-    let person_iter = stmt.query_map([], |row| {
-        Ok(Person {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            data: row.get(2)?,
-        })
-    })?;
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
+    for i in 1..20 {
+        let row1 = Example {
+            id: i,
+            name: format!("first name = {}", i * 100).into(),
+        };
+        connection.execute(
+            "INSERT INTO example (id, name) VALUES (:id, :name)",
+            to_params_named(&row1).unwrap().to_slice().as_slice(),
+        )?;
     }
+
+    let mut statement = connection.prepare("SELECT * FROM example")?;
+    let mut res = from_rows::<Example>(statement.query([])?);
+    for r in res {
+        println!("{}", serde_json::to_string(&r?)?);
+    }
+
+    /*let mut statement = connection.prepare("SELECT * FROM example")?;
+    let mut rows = statement.query_and_then([], from_row::<Example>)?;
+
+    let mut statement = connection.prepare("SELECT * FROM example")?;
+    let columns = columns_from_statement(&statement);
+    let mut rows =
+        statement.query_and_then([], |row| from_row_with_columns::<Example>(row, &columns))?;
+
+    let mut statement = connection.prepare("SELECT * FROM example")?;
+    let mut rows = statement.query([])?;
+    {
+        let mut res = from_rows_ref::<Example>(&mut rows);
+    }*/
     Ok(())
+}
+
+fn main() {
+    test().expect("");
 }
