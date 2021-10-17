358555ea333632b9df971e5cd51a05320e3edfd2
jongwhan <leejw51@gmail.com>
2021-10-17 15:22:01 +0900

    serialize

--- test_serialization/src/main.rs
+++ test_serialization/src/main.rs
@@ -1,60 +1,67 @@
 use serde::{Deserialize, Serialize};
-use std::time::SystemTime;
-#[derive(Serialize, Deserialize, Debug)]
-struct Point {
-    x: i32,
-    y: i32,
+use serde_json;
+mod test;
+#[derive(Serialize, Deserialize)]
+struct MyStruct {
+    #[serde(with = "from::string")]
+    biguint: u64,
+
+    #[serde(with = "from::string")]
+    bigint: i64,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-struct Entity {
-    x: f32,
-    y: f32,
-    t: SystemTime,
-}
+// Serde's 'with' attribute allows you to define a serializer and a deserializer in a module.
+// You can implement serialize and deserialize as functions in your code, without using modules,
+// and use the `deserialize_with` and `serialize_with` attributes instead.
+pub mod from {
+    // This is just syntactic sugar, so the attribute looks cool like "from::string".
+    // You don't need to embed multiple modules.
+    pub mod string {
+        use serde::{de::Error as _, Deserialize, Deserializer, Serialize, Serializer};
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-struct World(Vec<Entity>);
+        // This deserializer was originally written with u64 in mind. Then it was made generic by
+        // changing u64 to T everywhere and adding boundaries. Same with the serializer.
+        pub fn deserialize<'de, D, T>(deserializer: D) -> Result<T, D::Error>
+        where
+            D: Deserializer<'de>,
+            T: std::str::FromStr,
+            <T as std::str::FromStr>::Err: std::fmt::Display,
+        {
+            String::deserialize(deserializer)?
+                .parse::<T>()
+                .map_err(|e| D::Error::custom(format!("{}", e)))
+        }
 
-fn test1() {
-    let point = Point { x: 1, y: 2 };
-
-    // Convert the Point to a JSON string.
-    let serialized = serde_json::to_string(&point).unwrap();
-
-    // Prints serialized = {"x":1,"y":2}
-    println!("serialized = {}", serialized);
-
-    // Convert the JSON string back to a Point.
-    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
-
-    // Prints deserialized = Point { x: 1, y: 2 }
-    println!("deserialized = {:?}", deserialized);
+        pub fn serialize<S, T>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+            T: std::fmt::Display,
+        {
+            format!("{}", value).serialize(serializer)
+        }
+    }
 }
 
 fn main() {
-    let world = World(vec![
-        Entity {
-            x: 0.0,
-            y: 4.0,
-            t: SystemTime::now(),
-        },
-        Entity {
-            x: 10.0,
-            y: 20.5,
-            t: SystemTime::now(),
-        },
-    ]);
+    // Deserialize with custom deserializer
 
-    let encoded: Vec<u8> = bincode::serialize(&world).unwrap();
+    let incomingcustom = r#"
+    {
+      "biguint": "9007199254740992",
+      "bigint": "9007199254740994"
+    }
+    "#;
 
-    let decoded: World = bincode::deserialize(&encoded[..]).unwrap();
+    let customresult: MyStruct = serde_json::from_str(&incomingcustom).unwrap();
+    println!("biguint deserialized: {}", customresult.biguint);
+    println!("bigint deserialized: {}", customresult.bigint);
 
-    assert_eq!(world, decoded);
+    // Serialize with custom serializer
 
-    // Convert the Point to a JSON string.
-    let serialized = serde_json::to_string(&world).unwrap();
-
-    // Prints serialized = {"x":1,"y":2}
-    println!("serialized = {}", serialized);
+    let data = MyStruct {
+        biguint: 9007199254740993,
+        bigint: 9007199254740995,
+    };
+    println!("Serialized: {}", serde_json::to_string(&data).unwrap());
+    // Prints: Serialized: {"biguint":"9007199254740993","bigint":"9007199254740995"}
 }
