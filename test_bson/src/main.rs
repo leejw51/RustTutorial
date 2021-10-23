69621126014bb6592d65f390e092cb729792b941
jongwhan <leejw51@gmail.com>
2021-10-23 13:10:09 +0900

    bson

---
+++ test_bson/src/main.rs
@@ -0,0 +1,85 @@
+use bson::{bson, doc, oid, Bson, DateTime, Document};
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: i32,
+    phones: Vec<String>,
+}
+
+fn main2() {
+    // Some BSON input data as a `Bson`.
+    let bson_data: Bson = bson!({
+        "name": "John Doe",
+        "age": 43,
+        "phones": [
+            "+44 1234567",
+            "+44 2345678"
+        ]
+    });
+
+    let mut person: Person = bson::from_bson(bson_data).unwrap();
+
+    println!("Redacting {}'s record.", person.name);
+    person.name = "REDACTED".to_string();
+
+    let redacted_bson = bson::to_bson(&person).unwrap();
+}
+
+fn test() {
+    let string = Bson::String("hello world".to_string());
+    let int = Bson::Int32(5);
+    let array = Bson::Array(vec![Bson::Int32(5), Bson::Boolean(false)]);
+
+    let string: Bson = "hello world".into();
+    let int: Bson = 5i32.into();
+
+    let string = bson!("hello world");
+    let int = bson!(5);
+    let array = bson!([5, false]);
+}
+
+fn test2() {
+    let mut bytes = bson!(vec![Bson::Int32(5); 24]);
+    let apple = doc! {
+        "name":"apple][",
+        "price":20000,
+        "denom":"usd",
+        "image": bytes
+    };
+
+    let doc = doc! {
+       "string": "string",
+       "bool": true,
+       "i32": 5,
+       "doc": { "x": true },
+       "apple": apple
+    };
+
+    // attempt get values as untyped Bson
+    let none = doc.get("asdfadsf"); // None
+    let value = doc.get("string"); // Some(&Bson::String("string"))
+
+    // attempt to get values with explicit typing
+    let string = doc.get_str("string"); // Ok("string")
+    let subdoc = doc.get_document("doc"); // Some(Document({ "x": true }))
+    let error = doc.get_i64("i32"); // Err(...)
+    let a = doc.to_string();
+
+    let b = bson::to_vec(&doc).unwrap();
+    println!("{}", a);
+    println!("{}", hex::encode(&b));
+}
+
+fn main() {
+    let doc = doc! {
+        "bool": true,
+        "fruits":vec![Bson::Int32(0x00bbffff);10]
+    };
+
+    // attempt get values as untyped Bson
+    let a = doc.to_string();
+    let b = bson::to_vec(&doc).unwrap();
+    println!("{}", a);
+    println!("{}", hex::encode(&b));
+}
