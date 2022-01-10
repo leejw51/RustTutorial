3a2f574c1c1a0f4d6c57bda10abc5dfd8a8bcd2d
jongwhan lee <jonghwan@crypto.com>
2022-01-10 21:56:00 +0900

    OK

--- test_sync/src/main.rs
+++ test_sync/src/main.rs
@@ -5,12 +5,12 @@ use tokio::runtime::Handle;
 use tokio::io::AsyncReadExt;
 use crossbeam::channel;
 
-async fn async_get_name() -> Result<String, Box<dyn Error + Send + Sync>> {
+async fn async_get_name() -> anyhow::Result<String> {
     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     Ok("OK".into())
 }
 
-fn sync_get_name(handle: Handle) -> Result<String, Box<dyn Error + Send + Sync>> {
+fn sync_get_name(handle: Handle) -> anyhow::Result<String> {
     let (tx, rx) = channel::bounded(1);
     handle.spawn(async move {
         let score_res = async_get_name().await;
@@ -21,12 +21,8 @@ fn sync_get_name(handle: Handle) -> Result<String, Box<dyn Error + Send + Sync>>
 
 #[tokio::main]
  async fn main() {
-    println!("Hello world");
     let res = tokio::task::spawn_blocking(|| {
         let score = sync_get_name(tokio::runtime::Handle::current()).unwrap();
-        // print score
         println!("score: {}", score);
     }).await.unwrap();
-    // call get_score_sync
-  
 }
